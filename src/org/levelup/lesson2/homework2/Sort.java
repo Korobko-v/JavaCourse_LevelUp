8e61ac69abbdc2ca221582d27290623156cb98b1
Korobko-v <19kvd95@mail.ru>
2020-12-18 22:40:34 +0300

    import,loop,output

--- src/org/levelup/lesson2/homework2/Sort.java
+++ src/org/levelup/lesson2/homework2/Sort.java
@@ -1,7 +1,5 @@
 package org.levelup.lesson2.homework2;
 
-import java.util.TreeSet;
-
 public class Sort {
     public static void main(String[] args) {
         int a = 234;
@@ -14,12 +12,13 @@ public class Sort {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = nums.length - 1; j > i; j--) {
                 if (nums[j - 1] > nums[j]) {
-                    int tmp = nums[j - 1];
-                    nums[j - 1] = nums[j];
-                    nums[j] = tmp;
+                    int tmp = a;
+                    a = b;
+                    b = tmp;
                 }
             }
         }
-        System.out.println("Возрастающая последовательность чисел: " + nums[0] + "," + nums[1] + "," + nums[2]);
+
+        System.out.println("Возрастающая последовательность чисел: " + a + "," + b + "," + c);
     }
 }
