b9da08d33033910c48e7668ec9661707ab76ccd2
Korobko-v <19kvd95@mail.ru>
2020-12-18 20:16:27 +0300

    initial commit

---
+++ src/org/levelup/lesson2/homework2/Intervals.java
@@ -0,0 +1,12 @@
+package org.levelup.lesson2.homework2;
+
+import java.util.Random;
+
+public class Intervals {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = r.nextInt(151)+5;
+        System.out.println((a>25&&a<100) ? "Число " + a + " содержится в интервале (25,100)" :
+                "Число " + a + " не содержится в интервале (25,100)");
+    }
+}
