6c9d5d8536c5e98541539750b6eff8a4a82a2fb1
Korobko-v <19kvd95@mail.ru>
2020-12-18 19:51:48 +0300

    initial commit

---
+++ src/org/levelup/lesson2/homework2/EvenNumbers.java
@@ -0,0 +1,14 @@
+package org.levelup.lesson2.homework2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String isEven = (n%2==0) ? "Число " + n + " - чётное" : "Число " + n + " - нечётное" ;
+        System.out.println(isEven);
+    }
+}
