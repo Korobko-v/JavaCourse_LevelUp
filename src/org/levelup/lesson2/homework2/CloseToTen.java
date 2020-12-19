5ee2c863bf71685627b46ce0fc285e2aeb786d92
Korobko-v <19kvd95@mail.ru>
2020-12-19 11:18:42 +0300

    if

--- src/org/levelup/lesson2/homework2/CloseToTen.java
+++ src/org/levelup/lesson2/homework2/CloseToTen.java
@@ -7,20 +7,18 @@ public class CloseToTen {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число n: ");
         int n = sc.nextInt();
-        if (n <=0) {
             while (n <=0) {
                 System.out.print("Введите положительное число: ");
                 n = sc.nextInt();
             }
-        }
+
         System.out.print("Введите число m: ");
         int m = sc.nextInt();
-        if (m <=0) {
             while (m <=0) {
                 System.out.print("Введите положительное число: ");
                 m = sc.nextInt();
             }
-        }
+
         System.out.println((Math.abs(10-n) < Math.abs(10-m)) ?
                 "Ближаейшее к 10 число: " + n : "Ближаейшее к 10 число: " + m);
     }
