d7420df83a1ec50073aae043bdc40943341d9220
Korobko-v <19kvd95@mail.ru>
2020-12-19 23:44:54 +0300

    spaces

--- src/org/levelup/lesson2/homework2/Intervals.java
+++ src/org/levelup/lesson2/homework2/Intervals.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class Intervals {
     public static void main(String[] args) {
         Random r = new Random();
-        int a = r.nextInt(151)+5;
-        System.out.println((a>25&&a<100) ? "Число " + a + " содержится в интервале (25,100)" :
+        int a = r.nextInt(151) + 5;
+        System.out.println((a > 25 && a < 100) ? "Число " + a + " содержится в интервале (25,100)" :
                 "Число " + a + " не содержится в интервале (25,100)");
     }
 }
