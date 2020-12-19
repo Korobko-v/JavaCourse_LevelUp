ec678bbc6153f04f90bc979eb24fd8dd3dcde06c
Korobko-v <19kvd95@mail.ru>
2020-12-19 23:46:20 +0300

    spaces

--- src/org/levelup/lesson2/homework2/TheGreatestNumber.java
+++ src/org/levelup/lesson2/homework2/TheGreatestNumber.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class TheGreatestNumber {
     public static void main(String[] args) {
         Random r = new Random();
-        int a = r.nextInt(900)+100;
+        int a = r.nextInt(900) + 100;
         String[]strDigits = String.valueOf(a).split("");
         int maxDigit = 0;
         for (int i = 0; i < strDigits.length; i++) {
-            maxDigit = Math.max(Integer.parseInt(strDigits[i]),maxDigit);
+            maxDigit = Math.max(Integer.parseInt(strDigits[i]), maxDigit);
         }
 
         System.out.println("В числе " + a + " наибольшая цифра: " + maxDigit);
