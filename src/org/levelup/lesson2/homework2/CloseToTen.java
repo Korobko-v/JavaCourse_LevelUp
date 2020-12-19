78d73faf3a2e833120994d1981b751653280a576
Korobko-v <19kvd95@mail.ru>
2020-12-19 11:15:57 +0300

    positive numbers, output

--- src/org/levelup/lesson2/homework2/CloseToTen.java
+++ src/org/levelup/lesson2/homework2/CloseToTen.java
@@ -7,8 +7,21 @@ public class CloseToTen {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число n: ");
         int n = sc.nextInt();
+        if (n <=0) {
+            while (n <=0) {
+                System.out.print("Введите положительное число: ");
+                n = sc.nextInt();
+            }
+        }
         System.out.print("Введите число m: ");
         int m = sc.nextInt();
-        System.out.println((Math.abs(10-n) < Math.abs(10-m)) ? n : m);
+        if (m <=0) {
+            while (m <=0) {
+                System.out.print("Введите положительное число: ");
+                m = sc.nextInt();
+            }
+        }
+        System.out.println((Math.abs(10-n) < Math.abs(10-m)) ?
+                "Ближаейшее к 10 число: " + n : "Ближаейшее к 10 число: " + m);
     }
 }
