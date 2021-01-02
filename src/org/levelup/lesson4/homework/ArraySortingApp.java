c2f22fec423d27fbe12d1dbc020bd327f56e0b0f
Korobko-v <19kvd95@mail.ru>
2021-01-02 21:29:49 +0300

    sorting

--- src/org/levelup/lesson4/homework/ArraySortingApp.java
+++ src/org/levelup/lesson4/homework/ArraySortingApp.java
@@ -6,6 +6,7 @@ public class ArraySortingApp {
         Sorting sorting = new Sorting();
         int[] evenArr = new int[] { 2, 4, 1, 0, -18, 12};
         int[] oddArr = new int[] { 2, 4, 1, 0, -18, 20, 12};
+        int[] longArr = new int[] { 2, 4, 1, 0, -18, 20, 12, 8, 5, 3, 33, 34, 54, 233, 54, 34, 77, 88, 23, 25, 35, 90 };
         sorting.sort(evenArr);
         for (int i : evenArr) {
             System.out.println(i);
@@ -13,6 +14,10 @@ public class ArraySortingApp {
         sorting.sort(oddArr);
         for (int j : oddArr) {
             System.out.println(j);
+        }
+        sorting.sort(longArr);
+        for (int k : longArr) {
+            System.out.println(k);
         }
     }
 }
--- src/org/levelup/lesson4/homework/Sorting.java
+++ src/org/levelup/lesson4/homework/Sorting.java
@@ -1,11 +1,11 @@
 package org.levelup.lesson4.homework;
 
-// Массивы с чётной длиной сортирует слиянием, массивы с нечётной - пузырьком
+// Массивы с нечётной длиной менее 20 элементов сортирует пузырьком, остальные - слиянием
 
 public class Sorting {
     public int[] sort (int[] array) {
 
-        if (array.length % 2 == 0) {
+        if (array.length % 2 == 0 || array.length >= 20) {
            MergeSort ms = new MergeSort();
            ms.sort(array);
            System.out.println("Merge sorting");
